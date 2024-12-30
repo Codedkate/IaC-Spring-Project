ec8bccca7e2558111b7cb8f51f826162f60d90a9
codedkate <catherinejohn247@gmail.com>
2024-12-30 19:01:29 +0100

    update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
-resource "azurerm_resource_group" "myspringbootrg" {
-  name     = "myspringbootrg"
+resource "azurerm_resource_group" "SpringRG" {
+  name     = "springrg"
   location = "Central US"
 }
 
-
-resource "azurerm_kubernetes_cluster" "spring-boot-app-aks-cluster" {
-  name                = "spring-boot-app-aks-cluster"
-  location            = azurerm_resource_group.myspringbootrg.location
-  resource_group_name = azurerm_resource_group.myspringbootrg.name
-  dns_prefix          = "springbootappdnsaks"
+resource "azurerm_kubernetes_cluster" "Springcluster" {
+  name                = "springcluster1"
+  location            = azurerm_resource_group.SpringRG.location
+  resource_group_name = azurerm_resource_group.SpringRG.name
+  dns_prefix          = "springappaks"
 
   default_node_pool {
     name       = "default"
@@ -26,21 +25,21 @@ resource "azurerm_kubernetes_cluster" "spring-boot-app-aks-cluster" {
 }
 
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.Springclusterspring-boot-app-aks-cluster.kube_config[0].client_certificate
+  value     = azurerm_kubernetes_cluster.Springcluster.kube_config[0].client_certificate
   sensitive = true
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.spring-boot-app-aks-cluster.kube_config_raw
+  value = azurerm_kubernetes_cluster.Springcluster.kube_config_raw
 
   sensitive = true
 }
 
 #Registry
 
-resource "azurerm_container_registry" "myspringbootrg" {
-  name                = "springbootreactacr"
-  resource_group_name = azurerm_resource_group.myspringbootrg.name
-  location            = azurerm_resource_group.myspringbootrg.location
+resource "azurerm_container_registry" "springregistry" {
+  name                = "springcontainerregistry1"
+  resource_group_name = azurerm_resource_group.SpringRG.name
+  location            = azurerm_resource_group.SpringRG.location
   sku                 = "Premium"
 }
